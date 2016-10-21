150b144c10c2d46ef4d5ddc0c07ea976bf5b7cba
grapePrince <tul90@naver.com>
2016-10-21 14:52:46 +0900

    index

--- src/js/server.js
+++ src/js/server.js
@@ -8,24 +8,18 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3101);
 app.use(express.static(__dirname + '/public'));
 
-var fortuneCookies = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
-
 app.get('/', function(req, res) {
-	res.render('home');
+	res.render('index');
 });
+
+/*
 app.get('/about', function(req,res){
 	var randomFortune = 
 		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
 	res.render('about', { fortune: randomFortune });
 });
+*/
 
-// 404 catch-all handler (middleware)
 app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
